d3faf5d89c032350973684c2c44bdf29569f875f
parasite.ygy <parasite.ygy@gmail.com>
2023-09-12 17:07:10 +0900

    Project_Structure

---
+++ K-market/src/main/java/kr/co/kmarket/controller/member/JoinController.java
@@ -0,0 +1,19 @@
+package kr.co.kmarket.controller.member;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/join.do")
+public class JoinController extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/member/join.jsp");
+		dispatcher.forward(req, resp);	
+	}
+}
