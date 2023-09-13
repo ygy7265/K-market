ad9226cd647ce477f66920603ceded4a5c75a0a2
dlwlals6790 <dlwlals6790>
2023-09-13 14:02:49 +0900

    product, terms component setting

--- K-market/src/main/java/kr/co/kmarket/service/ProductService.java
+++ K-market/src/main/java/kr/co/kmarket/service/ProductService.java
@@ -1,5 +1,31 @@
 package kr.co.kmarket.service;
 
-public class ProductService {
+import java.util.List;
 
+import kr.co.kmarket.dao.ProductDAO;
+import kr.co.kmarket.dto.ProductDTO;
+
+public enum ProductService {
+	
+	INSTANCE;
+	
+	// productDAO
+	private ProductDAO dao = ProductDAO.getInstance();
+	
+	// service
+	public void insertProduct(ProductDTO dto) {
+		dao.insertProduct(dto);
+	}
+	public ProductDTO selectProduct(int prodNo) {
+		return dao.selectProduct(prodNo);
+	}
+	public List<ProductDTO> selectProducts() {
+		return dao.selectProducts();
+	}
+	public void updateProduct(ProductDTO dto) {
+		dao.updateProduct(dto);
+	}
+	public void deleteProduct(int prodNo) {
+		dao.deleteProduct(prodNo);
+	}
 }
