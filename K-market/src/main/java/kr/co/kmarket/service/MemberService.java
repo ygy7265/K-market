927fe4be4b3e24d9fd26bb97a60f14acc1625717
unknown <31daylee@gmail.com>
2023-09-13 14:41:57 +0900

    DAO/DTO/Service 최종 업데이트 완료

--- K-market/src/main/java/kr/co/kmarket/service/MemberService.java
+++ K-market/src/main/java/kr/co/kmarket/service/MemberService.java
@@ -1,5 +1,34 @@
 package kr.co.kmarket.service;
 
-public class MemberService {
+import java.util.List;
+import kr.co.kmarket.dao.MemberDAO;
 
+public enum MemberService {
+	
+	INSTANCE;
+	
+	private MemberDAO dao = MemberDAO.getInstance();
+	
+	
+	public void insertMember(MemberDAO dto) {
+		dao.insertMember(dto);
+	}
+	
+	public MemberDAO selectMember(String uid) {
+		return dao.selectMember(uid);
+	}
+	
+	public List<MemberDAO> selectMembers() {
+		return dao.selectMembers();
+	}
+	
+	public void updateMember(MemberDAO dto) {
+		dao.updateMember(dto);
+	}
+	
+	public void deleteMember(String uid) {
+		dao.deleteMember(uid);
+	}
+	
+	
 }
--- K-market/src/main/java/kr/co/kmarket/service/OrderService.java
+++ K-market/src/main/java/kr/co/kmarket/service/OrderService.java
@@ -1,5 +1,32 @@
 package kr.co.kmarket.service;
 
-public class OrderService {
+import java.util.List;
 
+import kr.co.kmarket.dao.OrderDAO;
+
+public enum OrderService {
+	INSTANCE;
+	
+	private OrderDAO dao = OrderDAO.getInstance();
+	
+	
+	public void insertOrder(OrderDAO dto) {
+		dao.insertOrder(dto);
+	}
+	
+	public OrderDAO selectOrder(String ordNo) { // 편의를 위해서 int ordNo 가 아닌 String 으로 설정해둠 
+		return dao.selectOrder(ordNo);
+	}
+	
+	public List<OrderDAO> selectOrders() {
+		return dao.selectOrders();
+	}
+	
+	public void updateOrder(OrderDAO dto) {
+		dao.updateOrder(dto);
+	}
+	
+	public void deleteOrder(String ordNo) {
+		dao.deleteOrder(ordNo);
+	}
 }
