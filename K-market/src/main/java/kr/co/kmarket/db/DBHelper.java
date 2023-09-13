543d3c5f739d0f2cd26e2642c4552f30243ba0b7
dlwlals6790 <dlwlals6790>
2023-09-13 17:34:27 +0900

    DBhelper

--- K-market/src/main/java/kr/co/kmarket/db/DBHelper.java
+++ K-market/src/main/java/kr/co/kmarket/db/DBHelper.java
@@ -1,5 +1,51 @@
 package kr.co.kmarket.db;
 
-public class DBHelper {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+public class DBHelper {
+	
+	protected PreparedStatement psmt = null;
+	protected Statement stmt = null;
+	protected Connection conn = null;
+	protected ResultSet rs = null;
+	
+	// DB 불러오기
+	public Connection getConnection() {
+		
+		try {
+			Context ctx = (Context) new InitialContext().lookup("java:comp/env");
+			DataSource ds = (DataSource) ctx.lookup("jdbc/Kmarket");
+			
+			conn = ds.getConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+	
+	// 닫기
+	public void close() throws SQLException {
+		if(rs != null) {
+			rs.close();
+		}
+		if(stmt != null) {
+			stmt.close();
+		}
+		if(psmt != null) {
+			psmt.close();
+		}
+		if(conn != null) {
+			conn.close();
+		}
+	}
 }
