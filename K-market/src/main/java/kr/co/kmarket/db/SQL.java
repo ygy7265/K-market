036dfa0f83c9daa583ec2de4be6a06b630043391
dlwlals6790 <dlwlals6790>
2023-09-14 17:32:51 +0900

    register seller

--- K-market/src/main/java/kr/co/kmarket/db/SQL.java
+++ K-market/src/main/java/kr/co/kmarket/db/SQL.java
@@ -23,7 +23,27 @@ public class SQL {
 													+ "`addr2`=?, "
 													+ "`regip`=?, "
 													+ "`rdate`=NOW() ";
+	public static final String INSERT_MEMBER_SELLER = "INSERT INTO `km_member` SET "
+													+ "`uid`=?, "
+													+ "`pass`=?, "
+													+ "`gender`=0, "
+													+ "`type`=2, "
+													+ "`company`=?, "
+													+ "`Name`=?, "
+													+ "`bizRegNum`=?, "
+													+ "`comRegNum`=?, "
+													+ "`hp`=?, "
+													+ "`fax`=?, "
+													+ "`email`=?, "
+													+ "`zip`=?, "
+													+ "`addr1`=?, "
+													+ "`addr2`=?, "
+													+ "`manager`=?, "
+													+ "`managerHp`=?, "
+													+ "`regip`=?, "
+													+ "`rdate`=NOW() ";
 	public static final String SELECT_COUNT_UID = "SELECT COUNT(*) FROM `km_member` WHERE `uid`=?";
 	public static final String SELECT_COUNT_EMAIL = "SELECT COUNT(*) FROM `km_member` WHERE `email`=?";
-	public static final String SELECT_COUNT_HP = "SELECT COUNT(*) FROM `km_member` WHERE `hp`=?";	
+	public static final String SELECT_COUNT_HP = "SELECT COUNT(*) FROM `km_member` WHERE `hp`=?";
+	
 }
