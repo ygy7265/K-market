b9c5e5b5fc4e7ba442fa03c2ffa55033343eb4a9
iisk145 <iisk145@gmail.com>
2023-09-14 12:43:43 +0900

    카테고리1,2DTO,헤더

--- K-market/src/main/java/kr/co/kmarket/controller/admin/IndexController.java
+++ K-market/src/main/java/kr/co/kmarket/controller/admin/IndexController.java
@@ -8,8 +8,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @WebServlet("/admin/index.do")
 public class IndexController extends HttpServlet{
+	private static final long serialVersionUID = 137123L;
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/admin/index.jsp");
